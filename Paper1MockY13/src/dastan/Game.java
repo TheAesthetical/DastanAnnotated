b70016ace17fe1a71e691034b001e550194940bd
nathanielroumana <nathanielroumana@localhost>
2024-03-06 15:39:09 +0000

    essentially finished, maybe more comments to come

--- Paper1MockY13/src/dastan/Game.java
+++ Paper1MockY13/src/dastan/Game.java
@@ -16,10 +16,9 @@ import java.util.Random;
 // Game
 //================================================================================================
 
-// Game class which acts as the main class
+// Game class which contains main
 public class Game
 {
-
 	//+++++++++++++++++++++++++++++++++++++++++++++++
 	// Main
 	//+++++++++++++++++++++++++++++++++++++++++++++++
@@ -45,6 +44,17 @@ public class Game
 
 class Dastan
 {
+	// List that stores the board as a list of squares
+	// Can be visualised as so:
+	//		    1  2  3  4  5  6  
+	//		   -------------------
+	//		 1 | 1| 2| 3| 4| 5| 6|
+	//		 2 | 7| 8| 9|10|11|12|
+	//		 3 |13|14|15|16|17|18|
+	//		 4 |19|20|21|22|23|24|
+	//		 5 |25|26|27|28|29|30|
+	//		 6 |31|32|33|34|35|36|
+	//		   -------------------
 	protected List <Square> board;
 
 	// Declaring the number of rows, columns and move option offer variables 
@@ -130,7 +140,7 @@ class Dastan
 			// For the number of columns 
 			for (int column = 1 ; column <= noOfColumns ; column++)
 			{
-				// Set the index of the square with the row and column concatinated in the loop
+				// Set the index of the square with the row and column concatenated in the loop
 				int index = getIndexOfSquare(row * 10 + column);
 				// Prints out a | plus the symbol at the index square
 				Console.write("|" + board.get(index).getSymbol());
@@ -190,14 +200,14 @@ class Dastan
 
 	private int getIndexOfSquare(int squareReference)
 	{
-		// Gets the tens column
+		// Gets the tens digit column of the reference
 		int row = squareReference / 10;
-		// Gets the ones column
+		// Gets the ones digit column of the reference
 		int col = squareReference % 10;
 
 		// Returns the ordered from left to right, top to bottom 
 		// (see diagram below for better demonstration)
-		// So an input of 36 would return 18
+		// So an input of 36 would return 18 [ROW][COL]
 		return (row - 1) * noOfColumns + (col - 1);
 
 		//		    1  2  3  4  5  6  
@@ -209,7 +219,7 @@ class Dastan
 		//		 5 |25|26|27|28|29|30|
 		//		 6 |31|32|33|34|35|36|
 		//		   -------------------
-		
+
 		// It does this because it is a LIST a stream of items that snakes across the board
 		// 1D data structure
 
@@ -310,7 +320,7 @@ class Dastan
 		// Variables hold whether the player has the opposite players mirza (won)
 		boolean player1HasMirza = false;
 		boolean player2HasMirza = false;
-		
+
 		// For all the squares in the board
 		for (Square s : board)
 		{
@@ -346,7 +356,8 @@ class Dastan
 
 		}
 
-		// Returns the opposite of both of 
+		// Returns the opposite of both of players mirza occupancy statuses
+		// This is because the original check doesn't check for the player capturing the opposite players mirza, and therefore does it here
 		return !(player1HasMirza && player2HasMirza);
 
 	}
@@ -355,7 +366,7 @@ class Dastan
 	{
 		// Selected square variable
 		int selectedSquare;
-		
+
 		// Gets the user to enter the square coordinates
 		Console.write("Enter the square " + description + " (row number followed by column number): ");
 		selectedSquare = Integer.parseInt(Console.readLine());
@@ -369,11 +380,11 @@ class Dastan
 	{
 		// Temporary variable to store the replaced choice in
 		int replaceChoice;
-		
+
 		//Asking the user to replace the current move from one in their queue
 		Console.write("Choose the move option from your queue to replace (1 to 5): ");
 		replaceChoice = Integer.parseInt(Console.readLine());
-		
+
 		// Updates the move option with the offer chosen (- 1 to get to 0 index) and creates the move option for the current offer using the position of the offer and the direction of the player
 		currentPlayer.updateMoveOptionQueueWithOffer(replaceChoice - 1 , createMoveOption(moveOptionOffer.get(moveOptionOfferPosition) , currentPlayer.getDirection()));
 		// Sets the players score
@@ -394,7 +405,7 @@ class Dastan
 			scoreAdjustment += (s.getPointsForOccupancy(currentPlayer));
 
 		}
-		
+
 		// Returns the score adjustment based on the calculation done above
 		return scoreAdjustment;
 
@@ -416,7 +427,7 @@ class Dastan
 			return board.get(getIndexOfSquare(finishSquareReference)).getPieceInSquare().getPointsIfCaptured();
 
 		}
-		
+
 		// Otherwise, it is empty, and therefore it will return no points
 		return 0;
 
@@ -454,7 +465,7 @@ class Dastan
 
 				// While the choice is less than 1 or greater than 3 (not valid)
 			} while (choice < 1 || choice > 3);
-			
+
 			// Reference for the starting square
 			int startSquareReference = 0;
 
@@ -512,13 +523,17 @@ class Dastan
 				currentPlayer = players.get(0);
 
 			}
-			
+
 			// Updates game over status required for the loop to continue/stop the game
 			gameOver = checkIfGameOver();
 
 		}
-		
+
+		// Broken the loop, so somebody has won the game and the game is over
+
+		// Displays the current state of the game
 		displayState();
+		// Prints out who has won the game, or if it is a draw
 		displayFinalResult();
 
 	}
@@ -532,17 +547,19 @@ class Dastan
 
 	private void displayFinalResult()
 	{
-
+		// If player 1s score equals player 2s score then its a draw
 		if (players.get(0).getScore() == players.get(1).getScore())
 		{
 			Console.writeLine("Draw!");
 
 		}
+		// If player 1s score is greater than player 2s score then player 1 is the winner
 		else if (players.get(0).getScore() > players.get(1).getScore())
 		{
 			Console.writeLine(players.get(0).getName() + " is the winner!");
 
 		}
+		// Otherwise player 2 is the winner
 		else
 		{
 			Console.writeLine(players.get(1).getName() + " is the winner!");
@@ -865,6 +882,7 @@ class Piece
 
 	public Piece(String t , Player b , int p , String s)
 	{
+		// Sets the type of piece it is, who it belongs to, the points if it is captured and the symbol on the board
 		typeOfPiece = t;
 		belongsTo = b;
 		pointsIfCaptured = p;
@@ -921,9 +939,10 @@ class Square
 
 	public Square()
 	{
-		// Resetting variables
+		// Initialising variables of the piece in the square, which player the piece belongs to and its symbol on the board
 		pieceInSquare = null;
 		belongsTo = null;
+		// Space " " as the board prints it as so
 		symbol = " ";
 
 	}
@@ -940,8 +959,11 @@ class Square
 
 	public Piece removePiece()
 	{
+		// The piece in the square is put in an object to be returned
 		Piece pieceToReturn = pieceInSquare;
+		// The piece in the square is then reset to null
 		pieceInSquare = null;
+		// And the removed piece returned to the program
 		return pieceToReturn;
 
 	}
@@ -972,12 +994,13 @@ class Square
 
 	public boolean containsKotla()
 	{
-
+		// If it contains a kotla symbol (either of them) then it returns the presence as true
 		if (symbol.equals("K") || symbol.equals("k"))
 		{
 			return true;
 
 		}
+		// Otherwise, it is false
 		else
 		{
 			return false;
@@ -1017,37 +1040,43 @@ class Kotla extends Square
 	@Override
 	public int getPointsForOccupancy(Player currentPlayer)
 	{
-
+		// If there is no piece in the square that the kotla is in (null) then there are no points
 		if (pieceInSquare == null)
 		{
 			return 0;
 
 		}
+		// If the square that the kotla is in belongs to the same player as the current player
 		else if (belongsTo.sameAs(currentPlayer))
 		{
-
+			// If the current player is the same as the player who owns the piece in the square, and the piece is either a regular piece of a mirza
 			if (currentPlayer.sameAs(pieceInSquare.getBelongsTo()) && (pieceInSquare.getTypeOfPiece().equals("piece") || pieceInSquare.getTypeOfPiece().equals("mirza")))
 			{
+				// Then 5 points are awarded
 				return 5;
 
 			}
 			else
 			{
+				// Otherwise no points are awarded
 				return 0;
 
 			}
 
 		}
+		// Otherwise, if it doesn't
 		else
 		{
-
+			// If the current player is the same as the player who owns the piece in the square, and the piece is either a regular piece of a mirza
 			if (currentPlayer.sameAs(pieceInSquare.getBelongsTo()) && (pieceInSquare.getTypeOfPiece().equals("piece") || pieceInSquare.getTypeOfPiece().equals("mirza")))
 			{
+				// Then 1 point is awarded
 				return 1;
 
 			}
 			else
 			{
+				// Otherwise no points are awarded
 				return 0;
 
 			}
@@ -1077,6 +1106,7 @@ class MoveOption
 
 	public MoveOption(String n)
 	{
+		// Sets the name of the  move option and initialises the list of moves
 		name = n;
 		possibleMoves = new ArrayList <>();
 
@@ -1088,11 +1118,12 @@ class MoveOption
 
 	public void addToPossibleMoves(Move m)
 	{
+		// Adds the move to the list of possible moves
 		possibleMoves.add(m);
 
 	}
 
-	// Getter for the name of the move
+	// Getter for the name of the move option
 	public String getName()
 	{
 		return name;
@@ -1119,7 +1150,7 @@ class MoveOption
 			}
 
 		}
-	
+
 		// Otherwise it is not (false)
 		return false;
 
@@ -1180,21 +1211,29 @@ class MoveOptionQueue
 
 	public String getQueueAsString()
 	{
+		// String to be concatenated with items from the queue and returned
 		String queueAsString = "";
+		// Item index counter to be appended to items in the returned string
 		int count = 1;
 
+		// For the number of move option items in the queue
 		for (MoveOption m : queue)
 		{
+			// Queue string is concatenated with the number of the item, with a dot, the current move option in the queue and a tab to separate items
 			queueAsString += count + ". " + m.getName() + "   ";
+			// Queue item counter is incremented by 1 ready for the next item in the queue
 			count += 1;
 
 		}
+
+		// Returns the completed concatenated queue as a string
 		return queueAsString;
 
 	}
 
 	public void add(MoveOption newMoveOption)
 	{
+		// Adds a new move option to the queue
 		queue.add(newMoveOption);
 
 	}
@@ -1236,8 +1275,11 @@ class MoveOptionQueue
 
 class Player
 {
+	// Stores the name of the player
 	private String name;
+	// Stores the board and direction of the player
 	private int direction , score;
+	// Stores the move option queue for the player
 	private MoveOptionQueue queue = new MoveOptionQueue();
 
 	//===============================================
@@ -1250,6 +1292,7 @@ class Player
 		// Starting score
 		score = 100;
 
+		// Sets the name and direction
 		name = n;
 		direction = d;
 
@@ -1261,17 +1304,19 @@ class Player
 
 	public boolean sameAs(Player APlayer)
 	{
-
+		// Null validation case
 		if (APlayer == null)
 		{
 			return false;
 
 		}
+		// If the players name equals the player objects name then it is the same player
 		else if (APlayer.getName().equals(name))
 		{
 			return true;
 
 		}
+		// Otherwise it is not the same player
 		else
 		{
 			return false;
@@ -1282,6 +1327,7 @@ class Player
 
 	public String getPlayerStateAsString()
 	{
+		// Returns the players entire state
 		return name + System.lineSeparator() + "Score: " + score + System.lineSeparator() + "Move option queue: " + queue.getQueueAsString() + System.lineSeparator();
 
 	}
